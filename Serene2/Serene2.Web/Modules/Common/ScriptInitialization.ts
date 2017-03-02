ffb49d358e3b904712058e023f4b9b4e353fedea
DEONA <DEONA@PAULLAPTOP>
2017-03-02 17:25:43 +0200

    Add project files.

---
+++ Serene2/Serene2.Web/Modules/Common/ScriptInitialization.ts
@@ -0,0 +1,5 @@
+﻿namespace Serene2.ScriptInitialization {
+    Q.Config.responsiveDialogs = true;
+    Q.Config.rootNamespaces.push('Serene2');
+    Serenity.EntityDialog.defaultLanguageList = LanguageList.getValue;
+}
